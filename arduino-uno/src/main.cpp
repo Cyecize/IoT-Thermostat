2247eb644edbabe47857d315a346b3eec59b1f0d
Tsvetelin Yakimov <ceci2205@abv.bg>
2024-08-18 21:02:27 +0300

    Clean up code.

--- arduino-uno/src/main.cpp
+++ arduino-uno/src/main.cpp
@@ -5,38 +5,40 @@
 const float TARGET_TEMP = 25.1;
 const float TRIGGER_TEMP = 28.0;
 const int SWITCH_PIN = 13;
+const int BAUD_RATE = 9600;
 
 const int THERMISTOR_PIN = 0; // Thermistor input pin
-int Vo; // Analog signal read from Thermistor
-float R1 = 10000.0; // Fixed resistance value in the circuit
-float R2, logR2, T; // Variables for resistance, log value, and temperature
+int thermistorData; // Analog signal read from Thermistor
+const float R1 = 10000.0; // Fixed resistance value in the circuit
+float R2, logR2, temp; // Variables for resistance, log value, and temperature
 bool isOn = false;
 
 // https://www.thinksrs.com/downloads/programs/therm%20calc/ntccalibrator/ntccalculator.html
-const float c1 = 1.328191148e-03, c2 = 2.000112940e-04, c3 = 2.350560518e-07; // Coefficients for Steinhart-Hart equation
+// Coefficients for Steinhart-Hart equation
+const float c1 = 1.328191148e-03, c2 = 2.000112940e-04, c3 = 2.350560518e-07;
 
 void setup() {
-    Serial.begin(9600); // Begin serial communication at 9600 baud rate
+    Serial.begin(BAUD_RATE);
     Serial.println("Starting...");
 
     pinMode(SWITCH_PIN, OUTPUT);
 }
 
-void loop() {
-    Vo = analogRead(THERMISTOR_PIN); // Read the analog signal from the Thermistor
+void processData() {
+    thermistorData = analogRead(THERMISTOR_PIN); // Read the analog signal from the Thermistor
 
-    // Check if Vo is zero
-    if (Vo == 0) {
-        Serial.println("Vo is zero! Avoiding division by zero.");
+    // Check if thermistorData is zero
+    if (thermistorData == 0) {
+        Serial.println("thermistorData is zero! Avoiding division by zero.");
         delay(500);
         return;
     }
 
-    R2 = R1 * (1023.0 / (float) Vo - 1.0); // Compute Thermistor resistance from analog read value
+    R2 = R1 * (1023.0 / (float) thermistorData - 1.0); // Compute Thermistor resistance from analog read value
 
     // Print intermediate values for debugging
-    // Serial.print("Vo: ");
-    // Serial.println(Vo);
+    // Serial.print("thermistorData: ");
+    // Serial.println(thermistorData);
     // Serial.print("R2: ");
     // Serial.println(R2);
 
@@ -47,37 +49,36 @@ void loop() {
     // Serial.println(logR2);
 
     // Steinhart-Hart equation to compute temperature in Kelvin
-    T = c1 + (c2 * logR2) + (c3 * logR2 * logR2 * logR2);
+    temp = c1 + (c2 * logR2) + (c3 * logR2 * logR2 * logR2);
     // Serial.print("Steinhart-Hart raw value: ");
-    // Serial.println(T);
+    // Serial.println(temp);
 
-    if (T != 0) {
-        T = 1.0 / T;
+    temp = 1.0 / temp;
 
-        // Print temperature in Kelvin
-        // Serial.print("Temperature in Kelvin: ");
-        // Serial.println(T);
+    // Print temperature in Kelvin
+    // Serial.print("Temperature in Kelvin: ");
+    // Serial.println(temp);
 
-        T = T - 273.15; // Convert temperature from Kelvin to Celsius
+    temp = temp - 273.15; // Convert temperature from Kelvin to Celsius
 
-        // Print temperature in Celsius
-        Serial.print("Temperature in Celsius: ");
-        Serial.println(T);
+    // Print temperature in Celsius
+    Serial.print("Temperature in Celsius: ");
+    Serial.println(temp);
 
-        if (T > TRIGGER_TEMP && !isOn) {
-            Serial.println("Turning on");
-            digitalWrite(SWITCH_PIN, HIGH);
-            isOn = true;
-        }
-
-        if (T < TARGET_TEMP && isOn) {
-            Serial.println("Turning off");
-            digitalWrite(SWITCH_PIN, LOW);
-            isOn = false;
-        }
-    } else {
-        Serial.println("Steinhart-Hart equation resulted in zero!");
+    if (temp > TRIGGER_TEMP && !isOn) {
+        Serial.println("Turning on");
+        digitalWrite(SWITCH_PIN, HIGH);
+        isOn = true;
     }
 
+    if (temp < TARGET_TEMP && isOn) {
+        Serial.println("Turning off");
+        digitalWrite(SWITCH_PIN, LOW);
+        isOn = false;
+    }
+}
+
+void loop() {
+    processData();
     delay(500); // Wait for 0.5 seconds before the next loop iteration
 }
